8990cd1877f7e431696b7c596079177b91d8bcd4
Anukriti Bagchi <91126891+anukriti-bagchi@users.noreply.github.com>
2021-09-22 13:27:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//functions
+//processing
+function add(numberOne, numberTwo){
+  console.log("numberOne: ",numberTwo, "numberTwo: ", numberTwo)
+ var sum = numberOne + numberTwo;
+ return sum;
+}
+
+//use the function
+var result = add(2,5);
+console.log("The sum of 2 and 5 is: " + result);
+ 
